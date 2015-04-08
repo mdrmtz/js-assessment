8dd2411b76b8543537f0bbbf99e6f821de1a430a
Daniel Ramos <daniel.ramos@bluetouch.mx>
2015-04-08 01:06:01 -0700

    Functions,Operators,Recursion and Regex

--- app/functions.js
+++ app/functions.js
@@ -3,39 +3,76 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+        return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+        return function(str2){
+            return str + ', ' + str2;
+        };
     },
 
     makeClosures : function(arr, fn) {
+        var makeClosures = [];
+        
+        var makeFn = function(val) {
+            return function() { return fn(val); };
+        };
 
+        for (var i = 0; i < arr.length; i++) {
+            makeClosures.push( makeFn(arr[i]) );
+        }
+        return makeClosures;
     },
 
     partial : function(fn, str1, str2) {
-
+        return function(arg) {
+            return fn.call(null, str1, str2, arg);
+        };
     },
 
     useArguments : function() {
-
+        var useArguments = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            useArguments = useArguments + arguments[i];
+        }
+        return useArguments;
     },
 
     callIt : function(fn) {
-
+        var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+        fn.apply(null, args);
     },
 
     partialUsingArguments : function(fn) {
-
+        var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+        return function() {
+            var moreArgs = args.concat(Array.prototype.slice.call(arguments));
+            return fn.apply(null, moreArgs);
+        };
     },
 
     curryIt : function(fn) {
+        function applyArguments(fn, args) {
+            return fn.apply(null, args);
+        }
+        function getArgumentAccumulator(accumulatedArguments, expectedArgumentsCount) {
+            return function (currentArgument) {
+                accumulatedArguments.push(currentArgument);
 
+                if (accumulatedArguments.length === expectedArgumentsCount) {
+                    return applyArguments(fn, accumulatedArguments);
+                } else {
+                    return getArgumentAccumulator(accumulatedArguments, expectedArgumentsCount);
+                }
+            };
+        }
+
+        return getArgumentAccumulator([], fn.length);
     }
   };
 });
--- app/recursion.js
+++ app/recursion.js
@@ -3,18 +3,90 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var listFiles = [];
+      var dirs = [];
 
+      parse(data);
+
+      function parse(directory) {
+        var file,
+            files = directory.files;
+
+        dirs.push( directory.dir );
+
+        for (var i = 0; i < files.length; i++) {
+            file = files[i];
+            if (typeof file === 'string') {
+                if (!dirName || dirs.indexOf(dirName) > -1) {
+                    listFiles.push(file);
+                }
+            } else {
+                parse(file);
+            }
+        }
+
+        dirs.pop();
+      }
+
+      return listFiles;
     },
 
-    permute: function(arr) {
+    permute: function(inputArr) {
+    // http://stackoverflow.com/a/11509565/54468
+    var results = [];
 
+      function permute(arr, memo) {
+        var cur;
+
+        for (var i = 0; i < arr.length; i++) {
+          memo = memo || [];
+          cur = arr.splice(i, 1);
+          if (arr.length === 0) {
+            results.push(memo.concat(cur));
+          }
+          permute(arr.slice(), memo.concat(cur));
+          arr.splice(i, 0, cur[0]);
+        }
+
+        return results;
+      }
+
+      return permute(inputArr);
     },
 
     fibonacci: function(n) {
+        function fib(n) {
+           if (n <= 1)
+              return n;
+           return fib(n-1) + fib(n-2);
+        }
 
+        return fib(n);
     },
 
     validParentheses: function(n) {
+        var validParentheses = [];
+        
+        brackets('', 0, 0, n);  
+        
+        function brackets(bracket, open, close, pairs)
+        {
+            if((open===pairs)&&(close===pairs))
+            {
+                validParentheses.push(bracket);
+            }
+            else
+            {
+                if(open<pairs){
+                    brackets(bracket + '(', open+1, close, pairs);
+                }
+                if(close<open){
+                    brackets(bracket + ')', open, close+1, pairs);
+                }
+            }
+        }
+       
+        return validParentheses;
 
     }
   };
